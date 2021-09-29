398b626965879ea26b4397ef6746cabbe00328d3
Muhammad Alif Akbar <90602823+alifakbar46@users.noreply.github.com>
2021-09-29 15:28:14 +0700

    WP2.Tugas-P3

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Muhammad Alif</li>
+		<li>Nama Belakang : Akbar</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+	    <li>Jalan Krapu No. 10 Ciledug</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+	    <li>Tangerang</li>
+	</ul>
+	
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+	    <li>Bulu tangkis</li>
+	    <li>Jogging</li>
+	</ul>            	
